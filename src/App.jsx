6a0f7b2e6019ab8ac54d1d7e8e1bd3d9c0bfd63b
Derian Molina Lopez <derianmolina220@gmail.com>
2024-03-29 11:28:04 -0700

    preparando repositorio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="font-bold text-5xl">Hola mundo</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index', './src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
