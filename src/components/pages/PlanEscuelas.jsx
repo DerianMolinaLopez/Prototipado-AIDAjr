3e244b393d374fe883b00cfa4fe4d7cfeb077271
Jose Ramses Aldama <ramchu_aldama@hotmail.com>
2024-05-17 10:02:05 -0700

    Plan de pagos y personajes

---
+++ src/components/pages/PlanEscuelas.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const PlanEscuelas=()=>{
+    return(
+        <div className="PlanInicio">
+                <section className='section01'>
+                    <article className='cartaPago'>
+                        <p className='letraPrincipal nito'>Plan para escuelas</p>
+                        <span className='descripcion'>El aprendizaje de herramientas de ofimatica a la palma de tu mano</span>
+                        <hr />
+                        <p className='letraPrincipal nito'>$Oferta educativa/6mo</p>
+                        <span className='descripcion'>Por convenios. contactos en cualquier momento</span>
+                        <ul className='elementosDesc'>
+                            <li className='puntosDesc'>Acceso a todas las herramientas</li>
+                            <li className='puntosDesc'>Cursos disponibles de mayor aprendizaje</li>
+                            <li className='puntosDesc'>Certificados de aprendizaje</li>
+                            <li className='puntosDesc'>Aprendizaje actualizado</li>
+                        </ul>
+                        <button className='boton nito'>INICIA AHORA</button>
+                    </article>
+                </section>
+                <section className='section02'>
+                    <article className="cartaDescriptiva">
+                        <p className='letraPrincipal'>Plan de acceso escolar</p>
+                        <hr />
+                        <span className='descCompra'>
+                        ¡Transforma la Educación con AIDA JR para Escuelas!
+                        ¿Estás buscando una solución integral que impulse el aprendizaje y la productividad en tu escuela? ¡AIDA JR para 
+                        Escuelas es la respuesta! Nuestro programa ofrece acceso completo a la versión de pago de AIDA JR, proporcionando
+                        a estudiantes y profesores una suite de herramientas poderosas para mejorar el rendimiento académico y simplificar 
+                        las tareas administrativas.
+                        </span>
+                        <p></p>
+                        <span className=''>
+                        ¡Únete a la revolución educativa con AIDA JR para Escuelas y lleva el aprendizaje y la productividad al siguiente
+                        nivel!
+                        </span>
+                    </article>
+                </section>
+            </div>
+    )
+}
+
+export default PlanEscuelas
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '../src/login.css'
+import '../src/cardNegocios.css'
 import Inicio from './components/pages/InicioCursos/Inicio.jsx'
 import PaginaExcel from './components/pages/PaginaExcel.jsx'
 import PaginaPower from './components/pages/PaginaPower.jsx'
 import PaginaWord from './components/pages/PaginaWord.jsx'
 import Login from './components/pages/Login.jsx'
 import PresentacionHerramientas from './components/PresentacionHerramientas.jsx'
+import Perfil from './components/cursos/Perfil.jsx'
+import PlanEscuelas from './components/pages/PlanEscuelas.jsx'
+import PlanPrueba from './components/pages/PlanPrueba.jsx'
+import PlanGenerales from './components/pages/PlanGenerales.jsx'
 import {createBrowserRouter,RouterProvider}  from 'react-router-dom'
 //el router para mandar todas las rutas
 const router  = createBrowserRouter([
@@ -35,15 +40,29 @@ const router  = createBrowserRouter([
     element:<PaginaPower/>
   },
   {
-
     path: '/AIDA-CURSOS',
     element:<Inicio/>
   },
-
-{
+  {
     path: '/Login',
     element:<Login/>
   },
+  {
+    path: '/Perfil',
+    element:<Perfil/>
+  },
+  {
+    path: '/PlanEscuelas',
+    element:<PlanEscuelas/>
+  },
+  {
+    path: '/PlanPrueba',
+    element:<PlanPrueba/>
+  },
+  {
+    path: '/PlanGenerales',
+    element:<PlanGenerales/>
+  }
 
 ])
 //? en este proyecto habra rutas y otras que si o si seran anidadas
